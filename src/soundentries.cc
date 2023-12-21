780774861274c2e752df585bb6379b4468044454
trcrsired <uwgghhbcad@gmail.com>
2023-12-21 07:33:40 -0500

    soundentries

--- src/soundentries.cc
+++ src/soundentries.cc
@@ -3,7 +3,7 @@ try
 {
 	using namespace fast_io::io;
 	auto soundentriesdb{::wdb2::read_db2<::wdb2::definitions::SoundEntriesRec>("SoundEntries.db2")};
-	::fast_io::obuf_file soundentrieslua("SoundEntriesToFiledataIDs.lua");
+	::fast_io::obuf_file soundentrieslua("SoundEntries.lua");
 	print(soundentrieslua,R"abc(local MusicBox = LibStub("AceAddon-3.0"):GetAddon("MusicBox")
 MusicBox.SoundEntries=
 {)abc");
@@ -32,18 +32,20 @@ MusicBox.SoundEntries=
 					print(soundentrieslua,e,",");
 				}
 			}
-			if(e.map_ID==36308)	//Orgrimmar Additional because we only have old versions of db2 file. These are guesses. Vulpera
+			if(e.m_ID==36308)	//Orgrimmar Additional because we only have old versions of db2 file. These are guesses. Vulpera
 			{
 				print(soundentrieslua,"2145688,2145690");
 			}// Orgrimmar Night
-			else if(e.map_ID==36309)
+			else if(e.m_ID==36309)
 			{
 				print(soundentrieslua,"2145689,2145694");
 			}
-			else if(e.map_ID==2532)	//Stormwind. AnduinPt1. No Pt2
+/*
+			else if(e.m_ID==2532)	//Stormwind. AnduinPt1. No Pt2
 			{
 				print(soundentrieslua,"1417240,1417241,1417242,1417243,1417244,1417245,1417246,1417247");
 			}
+*/
 			if(countnotone)
 			{
 				print(soundentrieslua,"},");
